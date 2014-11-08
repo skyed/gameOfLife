3162840d5f663f7c66f4e3ea8879c78b5600d999
Billy Wolfington <man_of_magik903@yahoo.com>
2014-11-08 17:10:55 -0500

    Starter code started

--- gol.js
+++ gol.js
@@ -1,10 +1,49 @@
-$(function ()
-{
-    $("#still-life-btn, #oscillator-btn, #spaceship-btn").click(function ()
-    {
+$(function () {
+	var gridArray = [];
+	
+
+	function getGrid() {
+		var c = document.getElementById("grid");
+		return c.getContext("2d");
+	}
+
+	function initGrid() {
+		var grid = getGrid();
+
+		
+	}
+
+
+	function initArray() {
+		
+	}
+
+    $("#still-life-btn, #oscillator-btn, #spaceship-btn").click(function () {
         var selector = $(this).attr("id");
         selector = "#" + selector.replace("btn", "select");
 
-        alert($(selector).val());
+        var pattern = $(selector).val();
+        drawPattern(pattern);
     });
+
+
+    /********************
+     *	YOUR CODE HERE  *
+     ********************/
+
+    function drawPattern(inputPattern) {
+
+    }
+
+    function drawStillLife() {
+
+    }
+
+    function drawOscillator() {
+
+    }
+
+    function drawSpaceship() {
+
+    }
 });
